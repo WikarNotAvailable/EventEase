22e2e60be4ebcdbfa01ac0467c1034495413c400
Wikar <72315779+WikarNotAvailable@users.noreply.github.com>
2023-05-16 22:07:30 +0200

    Wikar/user types (#3)
    * Wikar/UserTypes: added getters
    
    * deleted console logs from previous changes pushed to main
    
    * delete endpoint
    
    * Wikar/UserTypes: added update endpoint

--- Backend/src/userType/userTypeQueries.ts
+++ Backend/src/userType/userTypeQueries.ts
@@ -1,1 +1,5 @@
 export const addUserType = "INSERT INTO usertypes (name) VALUES ($1) RETURNING *";
+export const getUserTypes = "SELECT * FROM USERTYPES";
+export const getUserTypeById = "SELECT * FROM usertypes WHERE usertype_id = $1";
+export const deleteUserTypeById = "DELETE FROM usertypes WHERE usertype_id = $1";
+export const updateUserType = "Update usertypes SET name = $1 WHERE usertype_id = $2 RETURNING *";
--- Backend/src/userType/userTypeRoutes.ts
+++ Backend/src/userType/userTypeRoutes.ts
@@ -5,10 +5,24 @@ const userTypeRoutes : Router = Router();
 
 userTypeRoutes.post("/", controller.postUserType);
 
+userTypeRoutes.get("/", controller.getUserTypes);
+
+userTypeRoutes.get("/:id", controller.getUserTypeById);
+
+userTypeRoutes.delete("/:id", controller.deleteUser);
+
+userTypeRoutes.put("/:id", controller.updateUserType);
+
 export default userTypeRoutes;
 
 /*
 Body for Post
+
+{
+    "userTypeName": string,
+}
+
+Body for Update
 
 {
     "userTypeName": string,
