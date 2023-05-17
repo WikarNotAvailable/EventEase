5bbfacc40eb80184dd25e94c6f1be281e8ad122b
WOJT13 <wojtekpalka123@gmail.com>
2023-05-17 21:09:06 +0200

    Add Spot Type

---
+++ Backend/src/spotType/spotTypeController.ts
@@ -0,0 +1,84 @@
+import pool from "../../db"
+import { QueryResult } from "pg";
+import * as queries from "./spotTypeQueries";
+
+export const postSpotType = async (req: any,res: any) => {
+    console.log("test");
+    try{
+        const {spotTypeName} : Record<string, any> = req.body;
+        const newSpotType : QueryResult<any> = await pool.query(queries.addSpotType, [spotTypeName]);
+        
+        res.json(newSpotType.rows);
+    }catch(err: any){
+        res.status(500).json(err);
+        console.log(err);
+    }
+}
+
+export const getSpotType = async (req: any, res: any) => {
+    try{
+        pool.query(queries.getSpotType, (error, results) => {
+            if(error) throw error;
+
+            res.status(200).json(results.rows);
+        })
+    }catch(err:any){
+        return res.status(400).json(err);
+    }
+}
+
+export const getSpotTypeById = async (req: any, res: any) => {
+    try{
+        const id = parseInt(req.params.id);
+            
+        pool.query(queries.getSpotTypeById, [id], (error, result) => {
+            if(error) throw error;
+
+            if(result.rows.length){
+                res.status(200).json(result.rows);
+            }
+            else{
+                res.status(400).json({message: "Spot Type does not exist. (Non existent id)"})
+            }
+        })
+    }catch(err: any){
+        return res.status(400).json(err);
+    }
+}
+
+export const deleteSpotType = async (req: any,res: any) => {
+    try{
+        const id = parseInt(req.params.id);
+        const spotType: QueryResult<any> = await pool.query(queries.getSpotTypeById, [id]);
+
+        if(!spotType.rows.length){
+            res.status(400).json({message: "Spot type does not exist. (Non existent id)"})
+        }
+        else {
+            pool.query(queries.deleteSpotType, [id], (error, result) => {
+            if(error) throw error;
+            res.status(200).json({message: "Successfully deleted."});
+            })
+        }
+    }catch(err: any){
+        return res.status(400).json(err);
+    }
+}
+
+export const updateSpotType = async (req: any,res: any) => {
+    try{
+        const id = parseInt(req.params.id);
+        const {spotTypeName} = req.body;
+        const spotType: QueryResult<any> = await pool.query(queries.getSpotTypeById, [id]);
+
+        if(!spotType.rows.length){
+            res.status(400).json({message: " Spot type does not exist. (Non existent id)"})
+        }
+        else{
+            const newSpotType: QueryResult<any> = await pool.query(queries.updateSpotType, [spotTypeName, id]);
+            res.json(newSpotType.rows);
+        }
+    }catch(err: any){
+        return res.status(400).json(err);
+    }
+}
---
+++ Backend/src/spotType/spotTypeQueries.ts
@@ -0,0 +1,6 @@
+export const addSpotType = "INSERT INTO spottypes (name) VALUES ($1) RETURNING *";
+export const getSpotType = "SELECT * FROM spottypes"
+export const getSpotTypeById = "SELECT * FROM spottypes WHERE spottype_id = $1";
+export const deleteSpotType = "DELETE FROM  spottypes WHERE spottype_id = $1";
+export const updateSpotType = "UPDATE spottypes SET name = $1 WHERE spottype_id = $2 RETURNING *";
+
