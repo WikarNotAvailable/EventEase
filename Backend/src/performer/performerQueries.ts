df470295d0771c56cabbdd535f7ad31d38d08a62
Wojciech Dranka <dr.woj@wp.pl>
2023-05-20 19:15:01 +0200

    Fix Queries to return whole JSON object

--- Backend/src/performer/performerQueries.ts
+++ Backend/src/performer/performerQueries.ts
@@ -1,6 +1,6 @@
 export const addPerformer = "INSERT INTO performers (performertype_id, name, description) VALUES ($1, $2, $3) RETURNING *";
-export const getPerformers = "SELECT * FROM performers";
-export const getPerformerById = "SELECT * FROM performers WHERE performer_id = $1";
+export const getPerformers = "SELECT * FROM performers AS p JOIN performertypes AS t ON p.performertype_id = t.performertype_id";
+export const getPerformerById = "SELECT * FROM performers AS p JOIN performertypes AS t ON p.performertype_id = t.performertype_id WHERE performer_id = $1";
 export const deletePerformer = "DELETE FROM performers WHERE performer_id = $1";
 export const updatePerformer = "UPDATE performers SET performertype_id = $1, name = $2, description = $3 WHERE performer_id = $4 RETURNING *";
 export const checkPerformerNameExists = "SELECT * FROM performers WHERE name = $1";
