5f025e3f73ef553a2dc314f9aad714a7360f92c5
Wojciech Dranka <dr.woj@wp.pl>
2023-05-20 18:43:28 +0200

    Add routes for performers

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import userTypeRoutes from "./src/userType/userTypeRoutes";
 import userRoutes from "./src/user/userRoutes";
+import performerRoutes from "./src/performer/performerRoutes";
 
 const app: Express = express();
 
@@ -8,6 +9,8 @@ app.use(express.json())
 
 app.use("/api/users", userRoutes);
 app.use("/api/userTypes", userTypeRoutes);
+app.use("/api/performers", performerRoutes);
+
 
 app.listen(8000, () =>{
   console.log("Server is listening on port 5000");
