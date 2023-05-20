ce6d525f88e0db8558b079753229c21e7f114b6f
Wojciech Dranka <dr.woj@wp.pl>
2023-05-20 21:18:06 +0200

    Fix Endpoint path

--- Backend/src/eventsPerformers/eventsPerformersRoutes.ts
+++ Backend/src/eventsPerformers/eventsPerformersRoutes.ts
@@ -6,8 +6,8 @@ const eventsPerformersRoutes : Router = Router();
 
 eventsPerformersRoutes.post('/', controller.addEventPerformer);
 eventsPerformersRoutes.get('/', controller.getEventsPerformers);
-eventsPerformersRoutes.get('/:id', controller.getEventPerformerByEventId);
-eventsPerformersRoutes.get('/:id', controller.getEventPerformerByPerformerId);
+eventsPerformersRoutes.get('/event/:id', controller.getEventPerformerByEventId);
+eventsPerformersRoutes.get('/performer/:id', controller.getEventPerformerByPerformerId);
 eventsPerformersRoutes.delete('/:event_id/:performer_id', controller.deleteEventPerformer);
 
 export default eventsPerformersRoutes;
