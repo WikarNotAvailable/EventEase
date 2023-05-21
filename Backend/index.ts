76ad196ba04eb896e4f097d90c6b6edef90656da
WOJT13 <wojtekpalka123@gmail.com>
2023-05-21 13:49:44 +0200

    Add table in database and fix index

--- Backend/index.ts
+++ Backend/index.ts
@@ -4,6 +4,8 @@ import userRoutes from "./src/user/userRoutes";
 import spotTypeRoutes from "./src/spotType/spotTypeRoutes";
 import transactionStatusRoutes from "./src/transactionStatus/transactionStatusRoutes";
 import ticketTypeRoutes from "./src/ticketType/ticketTypeRoutes";
+//import eventTypeRoutes from "./src/eventType/eventTypeRoutes";
+import companyRoutes from "./src/company/companyRoutes";
 
 const app: Express = express();
 
@@ -14,6 +16,8 @@ app.use("/api/userTypes", userTypeRoutes);
 app.use("/api/spotTypes", spotTypeRoutes);
 app.use("/api/transactionStatuses", transactionStatusRoutes);
 app.use("/api/ticketTypes", ticketTypeRoutes);
+//app.use("/api/eventTypes", eventTypeRoutes);
+app.use("/api/companies", companyRoutes);
 
 app.listen(8000, () =>{
   console.log("Server is listening on port 8000");
