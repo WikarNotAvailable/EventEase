e74ab75b6e411dd4f787e993b4207075d0504517
Wojciech Palka <116636417+WOJT13@users.noreply.github.com>
2023-05-21 18:40:59 +0200

    Company endpoint (#13)
    * Company endpoint
    
    * Add table in database and fix index
    
    * Comment eventTypes
    
    * Delete eventTypes code from datebase and index
    
    ---------
    
    Co-authored-by: Wikar <72315779+WikarNotAvailable@users.noreply.github.com>

--- Backend/database.sql
+++ Backend/database.sql
@@ -49,6 +49,28 @@ CREATE TABLE tickettypes(
     type VARCHAR(100) NOT NULL UNIQUE
 );
 
+CREATE TABLE company(
+    company_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(100) NOT NULL UNIQUE,
+    discussion_id INTEGER
+);
+
+CREATE TABLE discussion(
+    discussion_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(100) NOT NULL UNIQUE,
+    company_id INTEGER
+);
+
+CREATE TABLE comments(
+    comments_id SERIAL PRIMARY KEY,
+    content VARCHAR(100) NOT NULL,
+    post_date Date NOT NULL,
+    user_id INTEGER,
+    discussion_id INTEGER
+);
+
 CREATE TABLE eventtypes(
     eventype_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
