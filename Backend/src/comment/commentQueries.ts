0590bf35f5f07c8741479bc726ae43d4208e3f9b
WOJT13 <wojtekpalka123@gmail.com>
2023-05-23 12:09:43 +0200

    Add a few queries

---
+++ Backend/src/comment/commentQueries.ts
@@ -0,0 +1,4 @@
+export const addComment = "INSERT INTO comments (content, post_date, user_id, discussion_id) VALUES $1,$2,$3,$4 RETURNING *";
+export const getComment = "SELECT * FROM comments";
+export const getCommentById = "SELECT * FROM comments WHERE comment_id = $1";
+export const getCommentByUser = "SELECT * FROM comments WHERE user_id =$1";
