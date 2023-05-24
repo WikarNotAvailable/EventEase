65473b7390eb2eb395c293c51e054b2eeeb1f959
Marcin Mitręga <m.mitrega27@gmail.com>
2023-05-24 20:04:08 +0200

    Change menu

--- frontend/src/components/pages/MyProfile/ProfileData.tsx
+++ frontend/src/components/pages/MyProfile/ProfileData.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { Flex, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Flex, Text, Button, Input } from '@chakra-ui/react';
 
 export const ProfileData = () => {
-  const user = {
-    name: 'Arkadiusz',
-    surname: 'Nowak',
-    birthDate: '1990-01-01',
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState('Arkadiusz');
+  const [surname, setSurname] = useState('Nowak');
+  const [birthDate, setBirthDate] = useState('1990-01-01');
+  const [phoneNumber, setPhoneNumber] = useState('123-123-321');
+  const [email] = useState('arkadiusz.nowak@gmail.com');
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
   };
 
   return (
@@ -22,12 +31,65 @@ export const ProfileData = () => {
       top="300%"
     >
       <Text textAlign="center" fontSize="32" fontWeight="bold" marginBottom="2">
-        {`${user.name} ${user.surname}`}
+        {isEditing ? (
+          <>
+            <Input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter name"
+              marginBottom="2"
+            />
+            <Input
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+              placeholder="Enter surname"
+              marginBottom="2"
+            />
+          </>
+        ) : (
+          `${name} ${surname}`
+        )}
       </Text>
 
       <Text textAlign="center" fontSize="20">
-        Birth Date: {user.birthDate}
+        {isEditing ? (
+          <Input
+            value={birthDate}
+            onChange={(e) => setBirthDate(e.target.value)}
+            placeholder="Enter birth date"
+            marginBottom="2"
+          />
+        ) : (
+          birthDate
+        )}
       </Text>
+
+      <Text textAlign="center" fontSize="20">
+        {isEditing ? (
+          <Input
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="Enter phone number"
+            marginBottom="2"
+          />
+        ) : (
+          phoneNumber
+        )}
+      </Text>
+
+      <Text textAlign="center" fontSize="20">
+        Email: {email}
+      </Text>
+
+      {isEditing ? (
+        <Button onClick={handleSave} marginTop="2">
+          Save
+        </Button>
+      ) : (
+        <Button onClick={handleEdit} marginTop="2">
+          Edit
+        </Button>
+      )}
     </Flex>
   );
 };
