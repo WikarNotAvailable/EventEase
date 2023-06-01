484e9b5be1ec69fb610ca7fedf5c0aaed4ed91df
WOJT13 <wojtekpalka123@gmail.com>
2023-06-01 12:20:49 +0200

    Remove checkdiscussion

--- Backend/src/company/companyController.ts
+++ Backend/src/company/companyController.ts
@@ -4,16 +4,11 @@ import * as queries from "./companyQueries";
 
 export const postCompany = async (req: any,res: any) => {
     try{
-        const {name, description, discussion_id} = req.body;
-        const discussionInDatabase: QueryResult<any> = await pool.query(queries.checkDiscussionExists, [discussion_id]);
-
-        if(discussionInDatabase.rows.length == 0){
-            return res.status(400).json({message: "Discussion does not exist."});
-        }
-        else{
-            const newCompany: QueryResult<any> = await pool.query(queries.addCompany, [name, description, discussion_id]);
+        const {name, description} = req.body;
+        
+            const newCompany: QueryResult<any> = await pool.query(queries.addCompany, [name, description]);
             res.status(201).json(newCompany.rows[0]);
-        }
+        
     }catch(err: any){
         return res.status(400).json(err);
     }
@@ -73,14 +68,14 @@ export const deleteCompany = async (req: any,res: any) => {
 export const updateCompany = async (req: any,res: any) => {
     try{
         const id = parseInt(req.params.id);
-        let {name, description, discussion_id} = req.body;
+        let {name, description} = req.body;
         const company: QueryResult<any> = await pool.query(queries.getCompanyById, [id]);
 
         if(!company.rows.length){
             res.status(400).json({message: "Company does not exist. (Non existent id)"})
         }
         else{
-            const newCompany: QueryResult<any> = await pool.query(queries.updateCompany, [name, description, discussion_id,id]);
+            const newCompany: QueryResult<any> = await pool.query(queries.updateCompany, [name, description,id]);
             res.json(newCompany.rows[0]);
         }
     }catch(err: any){
--- Backend/src/company/companyQueries.ts
+++ Backend/src/company/companyQueries.ts
@@ -1,6 +1,5 @@
 export const addCompany = "INSERT INTO companies (name, description) VALUES ($1,$2) RETURNING *";
 export const getCompany = "SELECT * FROM companies";
 export const getCompanyById = "SELECT companies.company_id, companies.name, companies.description, json_build_object('discussion_id', discussions.discussion_id, 'comments', json_build_object('comments_id', comments.comments_id,'content',comments.content)) AS discussion FROM companies LEFT JOIN discussions ON companies.discussion_id = discussions.discussion_id LEFT JOIN comments ON discussions.discussion_id = comments.discussion_id WHERE companies.company_id = $1";
-export const checkDiscussionExists ="SELECT * FROM discussions WHERE discussion_id = $1";
 export const deleteCompany = "DELETE FROM companies WHERE company_id = $1";
 export const updateCompany = "UPDATE companies SET name = $1,description = $2 WHERE company_id = $3 RETURNING *";
